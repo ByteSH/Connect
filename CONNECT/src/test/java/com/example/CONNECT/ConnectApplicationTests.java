9f8c109a840c3ca4b3fb132d84f66ac785ab981f
Hamza Shaikh <hamzash.byte@gmail.com>
2025-07-02 03:09:31 +0530

    Uploading the project file.

---
+++ CONNECT/src/test/java/com/example/CONNECT/ConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CONNECT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
